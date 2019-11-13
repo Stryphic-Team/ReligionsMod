682f153b0decb082cc63b06679abde33999a3b50
DeaSTL <jhartway99@gmail.com>
2019-11-13 14:31:49 -0500

    Added forge files

---
+++ src/main/java/com/stryphic/religionsmod/ReligionsMod.java
@@ -0,0 +1,28 @@
+package com.stryphic.religionsmod;
+
+import com.stryphic.religionsmod.utils.Reference;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class ReligionsMod
+{
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
---
+++ src/main/java/com/stryphic/religionsmod/utils/Reference.java
@@ -0,0 +1,7 @@
+package com.stryphic.religionsmod.utils;
+
+public class Reference {
+    public static final String MODID = "religionsmod";
+    public static final String NAME = "Religions Mod";
+    public static final String VERSION = "0.0.1";
+}
